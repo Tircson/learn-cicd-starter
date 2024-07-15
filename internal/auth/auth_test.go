d46c3746c0d0fca43188f3fcbcb5287403d6dc6e
github_username_here <email@example.com>
2024-07-15 15:34:08 +0000

    Test

--- internal/auth/auth_test.go
+++ internal/auth/auth_test.go
@@ -6,44 +6,51 @@ import (
     "testing"
 )
 
+
 func TestGetAPIKey(t *testing.T) {
     tests := []struct {
-        name    string
-        headers http.Header
-        want    string
-        wantErr error
+        name       string
+        headers    http.Header
+        want       string
+        wantErrMsg string
     }{
         {
-            name:    "valid header",
-            headers: http.Header{"Authorization": {"ApiKey few34t43tg"}},
-            want:    "few34t43tg",
-            wantErr: nil,
+            name:       "valid header",
+            headers:    http.Header{"Authorization": {"ApiKey few34t43tg"}},
+            want:       "few34t43tg",
+            wantErrMsg: "",
         },
         {
-            name:    "missing header",
-            headers: http.Header{},
-            want:    "",
-            wantErr: ErrNoAuthHeaderIncluded,
+            name:       "missing header",
+            headers:    http.Header{},
+            want:       "",
+            wantErrMsg: "no authorization header included",
         },
         {
-            name:    "malformed header",
-            headers: http.Header{"Authorization": {"Bearer few34t43tg"}},
-            want:    "",
-            wantErr: errors.New("malformed authorization header"),
+            name:       "malformed header",
+            headers:    http.Header{"Authorization": {"Bearer few34t43tg"}},
+            want:       "",
+            wantErrMsg: "malformed authorization header",
         },
         {
-            name:    "incomplete header",
-            headers: http.Header{"Authorization": {"ApiKey"}},
-            want:    "",
-            wantErr: errors.New("malformed authorization header"),
+            name:       "incomplete header",
+            headers:    http.Header{"Authorization": {"ApiKey"}},
+            want:       "",
+            wantErrMsg: "malformed authorization header",
         },
     }
 
     for _, tt := range tests {
         t.Run(tt.name, func(t *testing.T) {
             got, err := GetAPIKey(tt.headers)
-            if !errors.Is(err, tt.wantErr) {
-                t.Fatalf("expected error: %v, got: %v", tt.wantErr, err)
+            if err != nil {
+                if err.Error() != tt.wantErrMsg {
+                    t.Fatalf("expected error: %v, got: %v", tt.wantErrMsg, err.Error())
+                }
+            } else {
+                if tt.wantErrMsg != "" {
+                    t.Fatalf("expected error: %v, got no error", tt.wantErrMsg)
+                }
             }
             if got != tt.want {
                 t.Fatalf("expected: %v, got: %v", tt.want, got)
