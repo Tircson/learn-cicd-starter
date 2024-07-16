94f0710defb750ef704a98e3739ffe2affc4247e
github_username_here <email@example.com>
2024-07-16 19:27:43 +0000

    Test16

--- json.go
+++ json.go
@@ -27,5 +27,7 @@ func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 		return
 	}
 	w.WriteHeader(code)
-	w.Write(dat)
+	if _, err := w.Write(dat); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
 }
